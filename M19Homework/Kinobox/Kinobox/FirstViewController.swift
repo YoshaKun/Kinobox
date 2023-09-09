7917d85e331b21eab4b5effa24bf7f07f6115e53
YoshaKun <leshatatarincev911high@gmail.com>
2023-09-09 19:03:51 +0300

    UI completed

--- M19Homework/Kinobox/Kinobox/FirstViewController.swift
+++ M19Homework/Kinobox/Kinobox/FirstViewController.swift
@@ -9,39 +9,77 @@ import Foundation
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    let someButton = UIButton()
     
+    private let textField = UITextField()
+    private let findButton = UIButton()
+    private let popFilmsButton = UIButton()
+    private lazy var tableView = UITableView(frame: CGRect.zero, style: UITableView.Style.grouped)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
         
-        configureButton()
+        configureViews()
+        configureTableView()
         configureConstraints()
     }
     
-    private func configureButton() {
-        someButton.layer.cornerRadius = 10
-        someButton.setTitle("URLSession", for: .normal)
-        someButton.backgroundColor = .systemBlue
-        someButton.addTarget(self, action: #selector(didTappedSomeButton), for: .touchUpInside)
+    private func configureViews() {
+        textField.backgroundColor = .lightGray
+        textField.placeholder = "Тут пользовательский текст"
+        textField.borderStyle = .roundedRect
+        
+        findButton.layer.cornerRadius = 10
+        findButton.setTitle("Поиск", for: .normal)
+        findButton.backgroundColor = .systemBlue
+        findButton.addTarget(self, action: #selector(didTappedOnCell), for: .touchUpInside)
+        
+        popFilmsButton.layer.cornerRadius = 10
+        popFilmsButton.setTitle("Популярные фильмы", for: .normal)
+        popFilmsButton.backgroundColor = .systemBlue
+        popFilmsButton.addTarget(self, action: #selector(didTappedOnCell), for: .touchUpInside)
+    }
+    
+    private func configureTableView() {
+        
     }
     
     private func configureConstraints() {
-        view.addSubview(someButton)
-        someButton.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        findButton.translatesAutoresizingMaskIntoConstraints = false
+        popFilmsButton.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(textField)
+        view.addSubview(findButton)
+        view.addSubview(popFilmsButton)
+        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            someButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            someButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            someButton.widthAnchor.constraint(equalToConstant: 110),
-            someButton.heightAnchor.constraint(equalToConstant: 40),
+            textField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            textField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+            textField.heightAnchor.constraint(equalToConstant: 40),
+    
+            findButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 30),
+            findButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            findButton.heightAnchor.constraint(equalToConstant: 40),
+            findButton.widthAnchor.constraint(equalToConstant: 110),
+            
+            popFilmsButton.topAnchor.constraint(equalTo: findButton.bottomAnchor, constant: 30),
+            popFilmsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            popFilmsButton.heightAnchor.constraint(equalToConstant: 40),
+            popFilmsButton.widthAnchor.constraint(equalToConstant: 210),
+            
+            tableView.topAnchor.constraint(equalTo: popFilmsButton.bottomAnchor, constant: 30),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16)
         ])
     }
     
-    @objc private func didTappedSomeButton() {
+    @objc private func didTappedOnCell() {
         let vc = SecondViewController()
         navigationController?.present(vc, animated: true, completion: nil)
     }
--- M19Homework/Kinobox/Kinobox/SecondViewController.swift
+++ M19Homework/Kinobox/Kinobox/SecondViewController.swift
@@ -9,9 +9,138 @@ import Foundation
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    var kinopoiskLabel = UILabel()
+    var rateKinopoLabel = UILabel()
+    var imdbLabel = UILabel()
+    var rateImdb = UILabel()
+    
+    var imageFilm = UIImageView()
+    
+    var titleOfFilmRu = UILabel()
+    var titleOfFilmEn = UILabel()
+    var descripFilmLabel = UILabel()
+    var yearOfFilm = UILabel()
+    var durationOfFilm = UILabel()
+    
+    var labelStackView = UIStackView()
+    var mainStackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = .white
+        
+        configureViews()
+        configureStackViews()
+        configureConstraints()
     }
+    
+    private func configureViews() {
+        kinopoiskLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        kinopoiskLabel.text = "Kinopoisk"
+        kinopoiskLabel.textColor = .black
+        
+        rateKinopoLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        rateKinopoLabel.text = "рейтинг Kinopoisk"
+        rateKinopoLabel.textColor = .black
+        
+        imdbLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        imdbLabel.text = "IMDB"
+        imdbLabel.textColor = .black
+        
+        rateImdb.font = .systemFont(ofSize: 16, weight: .semibold)
+        rateImdb.text = "рейтинг IMDB"
+        rateImdb.textColor = .black
+        
+        imageFilm.backgroundColor = .red
+        
+        titleOfFilmRu.font = .systemFont(ofSize: 24, weight: .bold)
+        titleOfFilmRu.text = "Название фильма RU"
+        titleOfFilmRu.numberOfLines = 0
+        titleOfFilmRu.textAlignment = .center
+        titleOfFilmRu.textColor = .black
+        
+        titleOfFilmEn.font = .systemFont(ofSize: 16, weight: .semibold)
+        titleOfFilmEn.text = "Название фильма EN"
+        titleOfFilmEn.numberOfLines = 0
+        titleOfFilmEn.textAlignment = .center
+        titleOfFilmEn.textColor = .systemGray
+        
+        descripFilmLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+        descripFilmLabel.text = "Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма Описание фильма "
+        descripFilmLabel.numberOfLines = 0
+        descripFilmLabel.textColor = .black
+        
+        yearOfFilm.font = .systemFont(ofSize: 16, weight: .semibold)
+        yearOfFilm.text = "Год производства\nгод_"
+        yearOfFilm.numberOfLines = 0
+        yearOfFilm.textColor = .black
+        
+        durationOfFilm.font = .systemFont(ofSize: 16, weight: .semibold)
+        durationOfFilm.text = "Продолжительность\nвремя_мин."
+        durationOfFilm.numberOfLines = 0
+        durationOfFilm.textColor = .black
+    }
+    
+    private func configureStackViews() {
+        labelStackView.axis = .vertical
+        labelStackView.distribution = .fillEqually
+        labelStackView.alignment = .center
+        labelStackView.spacing = 16
+        
+        mainStackView.axis = .horizontal
+        mainStackView.spacing = 40
+    }
+    
+    private func configureConstraints() {
+        labelStackView.addArrangedSubview(kinopoiskLabel)
+        labelStackView.addArrangedSubview(rateKinopoLabel)
+        labelStackView.addArrangedSubview(imdbLabel)
+        labelStackView.addArrangedSubview(rateImdb)
+        mainStackView.addArrangedSubview(imageFilm)
+        mainStackView.addArrangedSubview(labelStackView)
+        view.addSubview(mainStackView)
+        view.addSubview(titleOfFilmRu)
+        view.addSubview(titleOfFilmEn)
+        view.addSubview(descripFilmLabel)
+        view.addSubview(yearOfFilm)
+        view.addSubview(durationOfFilm)
+        
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        imageFilm.translatesAutoresizingMaskIntoConstraints = false
+        titleOfFilmRu.translatesAutoresizingMaskIntoConstraints = false
+        titleOfFilmEn.translatesAutoresizingMaskIntoConstraints = false
+        descripFilmLabel.translatesAutoresizingMaskIntoConstraints = false
+        yearOfFilm.translatesAutoresizingMaskIntoConstraints = false
+        durationOfFilm.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            mainStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+            
+            imageFilm.heightAnchor.constraint(equalToConstant: 200),
+            imageFilm.widthAnchor.constraint(equalToConstant: 150),
+            
+            titleOfFilmRu.topAnchor.constraint(equalTo: mainStackView.bottomAnchor, constant: 30),
+            titleOfFilmRu.centerXAnchor.constraint(equalTo: mainStackView.centerXAnchor),
+
+            titleOfFilmEn.topAnchor.constraint(equalTo: titleOfFilmRu.bottomAnchor, constant: 15),
+            titleOfFilmEn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
+            descripFilmLabel.topAnchor.constraint(equalTo: titleOfFilmEn.bottomAnchor, constant: 30),
+            descripFilmLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            descripFilmLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+
+            yearOfFilm.topAnchor.constraint(equalTo: descripFilmLabel.bottomAnchor, constant: 30),
+            yearOfFilm.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            yearOfFilm.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+
+            durationOfFilm.topAnchor.constraint(equalTo: yearOfFilm.bottomAnchor, constant: 15),
+            durationOfFilm.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            durationOfFilm.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+        ])
+    }
+    
 }
